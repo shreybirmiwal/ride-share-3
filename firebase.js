e1652d9c245b5d98d684ebda1c0a2656fbfc7ebc
Shrey birmiwal <shreybirmiwal@gmail.com>
2024-02-17 15:11:28 -0600

    init

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuy2UNphIwKIqHaNCKvblQKHAVAGEWHwg",
+  authDomain: "ride-sjare.firebaseapp.com",
+  projectId: "ride-sjare",
+  storageBucket: "ride-sjare.appspot.com",
+  messagingSenderId: "909355138866",
+  appId: "1:909355138866:web:b9163a79efde8e2fa9a12b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ screens/Welcome.js
@@ -0,0 +1,97 @@
+import { View, Text, Pressable, Image } from 'react-native'
+import React from 'react'
+import { LinearGradient } from "expo-linear-gradient";
+import COLORS from '../constants/colors';
+import Button from '../components/Button';
+
+const Welcome = ({ navigation }) => {
+
+    return (
+        <LinearGradient
+            style={{
+                flex: 1
+            }}
+            colors={[COLORS.secondary, COLORS.primary]}
+        >
+            <View style={{ flex: 1 }}>
+
+                    <Image
+                            source={require("../assets/car-stock.png")}
+                            style={{
+                                height: 300,
+                                width: 300,
+                                transform: [
+                                    { translateX: 20 },
+                                    { translateY: 50 },
+                                    { rotate: "-15deg" }
+                                ]
+                            }}
+                            resizeMode='contain'
+                        />
+
+                <View style={{
+                    paddingHorizontal: 22,
+                    position: "absolute",
+                    top: 400,
+                    width: "100%"
+                }}>
+                    <Text style={{
+                        fontSize: 50,
+                        fontWeight: 800,
+                        color: COLORS.white
+                    }}>Welcome</Text>
+                    <Text style={{
+                        fontSize: 46,
+                        fontWeight: 800,
+                        color: COLORS.white
+                    }}>to rideShare</Text>
+
+                    <View style={{ marginVertical: 22 }}>
+                        <Text style={{
+                            fontSize: 16,
+                            color: COLORS.white,
+                            marginVertical: 4
+                        }}>text text text text text text text text </Text>
+                        <Text style={{
+                            fontSize: 16,
+                            color: COLORS.white,
+                        }}>text text text text text text text text g</Text>
+                    </View>
+
+                    <Button
+                        title="Join Now"
+                        onPress={() => navigation.navigate("Signup")}
+                        style={{
+                            marginTop: 22,
+                            width: "100%"
+                        }}
+                    />
+
+                    <View style={{
+                        flexDirection: "row",
+                        marginTop: 12,
+                        justifyContent: "center"
+                    }}>
+                        <Text style={{
+                            fontSize: 16,
+                            color: COLORS.white
+                        }}>Already have an account?</Text>
+                        <Pressable
+                            onPress={() => navigation.navigate("Login")}
+                        >
+                            <Text style={{
+                                fontSize: 16,
+                                color: COLORS.white,
+                                fontWeight: "bold",
+                                marginLeft: 4
+                            }}>Login</Text>
+                        </Pressable>
+
+                    </View>
+                </View>
+            </View>
+        </LinearGradient>
+    )
+}
+
+export default Welcome
